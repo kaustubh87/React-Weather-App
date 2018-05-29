739ed71f2eaea22de2a65a0e8d48a449750dae3e
kvin1087 <kaustubh87@gmail.com>
2018-05-29 15:29:49 -0700

    Added async await method to call Api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,12 +3,24 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = 'd3586f709f072881a1132e9043cf72ab';
+
 class App extends React.Component {
+
+    getWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call =  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+        const data = await api_call.json();
+        console.log(data);
+    }
+
     render() {
         return (
            <div>
               <Titles />
-              <Form />
+              <Form getWeather ={this.getWeather} />
               <Weather />
            </div>
         );

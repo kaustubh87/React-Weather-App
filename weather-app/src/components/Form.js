62e685835f3c49f1f53b0b23696f35a837d3fb8d
kvin1087 <kaustubh87@gmail.com>
2018-05-30 09:53:12 -0700

    Replaced Class components with functional components

--- weather-app/src/components/Form.js
+++ weather-app/src/components/Form.js
@@ -1,16 +1,27 @@
 import React from "react";
 
+/** Class Based */
+
+/*
 class Form extends React.Component{
 
     render(){
-        return(
-           <form onSubmit={this.props.getWeather}>
-               <input type="text" name="city" placeholder="City . . ." /><br />
-               <input type="text" name="country" placeholder="Country . . ." /><br />
-               <button>Get Weather</button>
-           </form>
-        )
+        
     }
 };
+
+*/
+
+/** Functional Components */
+
+const Form = (props) => {
+    return(
+        <form onSubmit={props.getWeather}>
+            <input type="text" name="city" placeholder="City . . ." /><br />
+            <input type="text" name="country" placeholder="Country . . ." /><br />
+            <button>Get Weather</button>
+        </form>
+     )
+}
 
 export default Form;
--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -21,13 +21,13 @@ import React from "react";
 
 const Weather = (props) => {
     return (
-    <div>
-        { props.city && props.country && <p>Location: {props.city}, {props.country}</p> }   
-        { props.temperature && <p> Temperature: {props.temperature}</p> }
-        { props.humidity && <p>Humidity: {props.humidity}</p> }
-        { props.description && <p> Conditions: {props.description}</p>}
-        { props.error && <p>{props.error}</p>}
-    </div>
+        <div>
+            { props.city && props.country && <p>Location: {props.city}, {props.country}</p> }   
+            { props.temperature && <p> Temperature: {props.temperature}</p> }
+            { props.humidity && <p>Humidity: {props.humidity}</p> }
+            { props.description && <p> Conditions: {props.description}</p>}
+            { props.error && <p>{props.error}</p>}
+        </div>
     );
 }
 

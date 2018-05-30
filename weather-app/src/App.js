f11bbad601447bd54c3a9e0844118dcb2e17a747
kvin1087 <kaustubh87@gmail.com>
2018-05-29 19:17:07 -0700

    Added state terms

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,6 +7,15 @@ const API_KEY = 'd3586f709f072881a1132e9043cf72ab';
 
 class App extends React.Component {
 
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    }
+
     getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
@@ -14,14 +23,30 @@ class App extends React.Component {
         const api_call =  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
         const data = await api_call.json();
         console.log(data);
+
+        this.setState({
+            temperature: data.main.temp,
+            city: data.name,
+            country: data.sys.country,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            error: ""
+        });
     }
 
     render() {
         return (
            <div>
               <Titles />
-              <Form getWeather ={this.getWeather} />
-              <Weather />
+              <Form getWeather = {this.getWeather} />
+              <Weather 
+              temperature= { this.state.temperature}
+              city = {this.state.city}
+              country = {this.state.country}
+              humidity = {this.state.humidity}
+              description = {this.state.description}
+              error = {this.state.error}
+              />
            </div>
         );
     }

7e7072cea94ead6a62d375f6af33283f34516138
kvin1087 <kaustubh87@gmail.com>
2018-05-30 09:30:34 -0700

    Used Functional React Component instead of Class based components

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-class Weather extends React.Component{
+/** Class based components */
+/*class Weather extends React.Component{
 
     render(){
         return(
@@ -14,5 +15,20 @@ class Weather extends React.Component{
         )
     }
 };
+*/
+
+/** Using React's Functional Component  */
+
+const Weather = (props) => {
+    return (
+    <div>
+        { props.city && props.country && <p>Location: {props.city}, {props.country}</p> }   
+        { props.temperature && <p> Temperature: {props.temperature}</p> }
+        { props.humidity && <p>Humidity: {props.humidity}</p> }
+        { props.description && <p> Conditions: {props.description}</p>}
+        { props.error && <p>{props.error}</p>}
+    </div>
+    );
+}
 
 export default Weather;
